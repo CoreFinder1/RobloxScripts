1995302f890d3f4e5d76da7854b0aa20591dbdc5
CoreFinder1 <93850973+CoreFinder1@users.noreply.github.com>
2025-10-17 22:19:24 +1100

    Update VDScript.lua

--- VDScript.lua
+++ VDScript.lua
@@ -12,3 +12,14 @@ local args = {
 	"Bandage"
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Shop"):WaitForChild("EquipItem"):FireServer(unpack(args))
+
+
+
+
+
+
+game.ReplicatedStorage.Remotes.Shop.EquipItem:FireServer("Gate");
+
+game.ReplicatedStorage.Remotes.Shop.EquipItem:FireServer("Motion Tracker");
+
+game.ReplicatedStorage.Remotes.Shop.EquipItem:FireServer("Bandage");
